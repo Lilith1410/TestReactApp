5b720a758d26f55583598d31ba2f89564a1cc471
Valerie <valerie.vaske@gmail.com>
2020-02-25 15:29:25 +0100

    deployed contract on ropsten + added infos to app

---
+++ frontend/src/config.js
@@ -0,0 +1,42 @@
+export const CONTRACT_ADDRESS = "0x6a8bb2f9B76A7D6093aba511C61a8a2E55a0091f";
+
+export const CONTRACT_ABI = [
+	{
+		"inputs": [],
+		"name": "incrementCounter",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"inputs": [],
+		"name": "counter",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "owner",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]; 
