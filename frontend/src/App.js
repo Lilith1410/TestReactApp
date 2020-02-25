dc5e4703be1848f878278428507dc5d5cf54025b
Valerie <valerie.vaske@gmail.com>
2020-02-25 15:32:16 +0100

    App.js aufgehübscht

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,19 @@ import React, { Component } from 'react';
 import './App.css';
 import { CONTRACT_ADDRESS, CONTRACT_ABI } from './config.js';
 import Torus from "@toruslabs/torus-embed";
-//import Counter from './pages/Counter.js';
-//import Header from './pages/Header.js';
-//import Footer from './pages/Footer.js';
 import Web3 from 'web3';
-//import Portis from '@portis/web3'
-//import {CONTRACT_ADDRESS, CONTRACT_ADDRESS_ABI} from './config.js';
 import { Box, Button, Heading, Text } from 'rimble-ui'
 
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      account: '',
+      counter: 0
+    };
+  }
 
   componentWillMount() {
     this.loadBlockchainData();
@@ -33,57 +36,17 @@ class App extends Component {
       showTorusButton: true // default: true
     });
     await torus.login(); // await torus.ethereum.enable()
+
     const web3 = new Web3(torus.provider);
-
-    //const infuraNode = {
-    //  nodeUrl: '',
-    //  chainId: ''
-    //}
-    //const portis = new Portis('96749b1d-1384-4d19-96fb-bad5a4a76fff', infuraNode)
-
-
-
-
-    // Skale
-    //const mySkaleChain = {
-    //  nodeUrl: "[ENDPOINT]",
-    //  chainId: 1,
-    //  nodeProtocol: 'rpc'
-    //}
-    // const portis = new Portis('96749b1d-1384-4d19-96fb-bad5a4a76fff', 'mySkaleChain')
-    // const portis = new Portis('96749b1d-1384-4d19-96fb-bad5a4a76fff', 'mainnet')
-    // const web3 = new Web3(portis.provider)
-     const accounts = await web3.eth.getAccounts()
-    // this.setState({ account: accounts[0] })
-
-    //web3.eth.getAccounts((error, accounts ) => {
-    //  console.log(accounts)
-    //})
-
-  //  const web3 = new Web3(Web3.givenProvider || 'https://localhost:8545')
-  //  const accounts = await web3.eth.getAccounts()
+    const accounts = await web3.eth.getAccounts()
     this.setState({ account: accounts[0] });
 
-    // set contract
-    //const testContract = new Web3.eth.Contract(CONTRACT_ADDRESS, CONTRACT_ADDRESS_ABI)
-  //  this.setState({ testContract })
-    //const count = await testContract.methods.getCounter().call()
-    //this.setState({ count })
-
     const contract = new web3.eth.Contract(CONTRACT_ABI, CONTRACT_ADDRESS)
-    this.setState({contract})
+    this.setState({contract};
 
     const getCounter = await contract.methods.counter().call()
     this.setState({ counter: getCounter })
 
-  }
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      account: '',
-      counter: 0
-    };
   }
 
   render() {
