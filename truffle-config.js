3f1c0dad5982b3a18318a2bd959300ae27ef58ba
Valerie <valerie.vaske@gmail.com>
2020-02-25 15:29:04 +0100

    added ropsten

--- truffle-config.js
+++ truffle-config.js
@@ -1,28 +1,8 @@
-/**
- * Use this file to configure your truffle project. It's seeded with some
- * common settings for different networks and features like migrations,
- * compilation and testing. Uncomment the ones you need or modify
- * them to suit your project as necessary.
- *
- * More information about configuration can be found at:
- *
- * truffleframework.com/docs/advanced/configuration
- *
- * To deploy via Infura you'll need a wallet provider (like truffle-hdwallet-provider)
- * to sign your transactions before they're sent to a remote public node. Infura accounts
- * are available for free at: infura.io/register.
- *
- * You'll also need a mnemonic - the twelve word phrase the wallet uses to generate
- * public/private key pairs. If you're publishing your code to GitHub make sure you load this
- * phrase from a file you've .gitignored so it doesn't accidentally become public.
- *
- */
+//const HDWalletProvider = require('truffle-hdwallet-provider');
+//const infuraKey = "fj4jll3k.....";
 
-// const HDWalletProvider = require('truffle-hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
-//
 // const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+//const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
   /**
@@ -43,8 +23,8 @@ module.exports = {
     },
     ropsten: {
       network_id: 3,
-      host: "localhost",
-      port: 8545,
+      host: "https://api.infura.io/v1/jsonrpc/ropsten",
+      port: 6688,
       gas: 2900000
     }
 
